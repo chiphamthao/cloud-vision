c7eddeef159d064d3d8341811454dc63e28b6658
Chi Pham <84998406+chiphamthao@users.noreply.github.com>
2022-06-30 04:35:59 -0400

    Add labels to class TextRecognitor

--- vision-cloud-ocr.py
+++ vision-cloud-ocr.py
@@ -1,65 +1,64 @@
 import io
 import os
 
-def detect_text(path):
-    """Detects text in the file."""
-    from google.cloud import vision
-    os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "/Users/macair/Downloads/delta-guild-354701-7d01beb6d1f3.json"
+class TextRecognitor: 
+    def __init__(self):
+        #super().__init__()
+        #self.initUI()
+        self.labels = []
+        
+    def detect_text(self, path):
+        """Detects text in the file."""
+        from google.cloud import vision
+        os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "/Users/macair/Downloads/delta-guild-354701-7d01beb6d1f3.json"
 
-    client = vision.ImageAnnotatorClient()
+        client = vision.ImageAnnotatorClient()
 
-    with io.open(path, 'rb') as image_file:
-        content = image_file.read()
+        with io.open(path, 'rb') as image_file:
+            content = image_file.read()
 
-    image = vision.Image(content=content)
+        image = vision.Image(content=content)
 
-    response = client.text_detection(image=image)
-    texts = response.text_annotations
-    print('Texts:')
+        response = client.text_detection(image=image)
+        texts = response.text_annotations
+        print('Texts:')
 
-    for text in texts:
-        print('\n"{}"'.format(text.description))
+        for text in texts:
+            self.labels.append('\n"{}"'.format(text.description))
 
-        vertices = (['({},{})'.format(vertex.x, vertex.y)
-                    for vertex in text.bounding_poly.vertices])
+            # vertices = (['({},{})'.format(vertex.x, vertex.y)
+            #             for vertex in text.bounding_poly.vertices])
 
-        print('bounds: {}'.format(','.join(vertices)))
+            # print('bounds: {}'.format(','.join(vertices)))
+    
+        if response.error.message:
+            raise Exception(
+                '{}\nFor more info on error messages, check: '
+                'https://cloud.google.com/apis/design/errors'.format(
+                    response.error.message))
 
-    if response.error.message:
-        raise Exception(
-            '{}\nFor more info on error messages, check: '
-            'https://cloud.google.com/apis/design/errors'.format(
-                response.error.message))
+    # def detect_text_uri(self, uri):
+    #     """Detects text in the file located in Google Cloud Storage or on the Web.
+    #     """
+    #     from google.cloud import vision
+    #     client = vision.ImageAnnotatorClient()
+    #     image = vision.Image()
+    #     image.source.image_uri = uri
 
-def detect_text_uri(uri):
-    """Detects text in the file located in Google Cloud Storage or on the Web.
-    """
-    from google.cloud import vision
-    client = vision.ImageAnnotatorClient()
-    image = vision.Image()
-    image.source.image_uri = uri
+    #     response = client.text_detection(image=image)
+    #     texts = response.text_annotations
+    #     print('Texts:')
 
-    response = client.text_detection(image=image)
-    texts = response.text_annotations
-    print('Texts:')
+    #     for text in texts:
+    #         print('\n"{}"'.format(text.description))
 
-    for text in texts:
-        print('\n"{}"'.format(text.description))
+    #         vertices = (['({},{})'.format(vertex.x, vertex.y)
+    #                     for vertex in text.bounding_poly.vertices])
 
-        vertices = (['({},{})'.format(vertex.x, vertex.y)
-                    for vertex in text.bounding_poly.vertices])
+    #         print('bounds: {}'.format(','.join(vertices)))
 
-        print('bounds: {}'.format(','.join(vertices)))
-
-    if response.error.message:
-        raise Exception(
-            '{}\nFor more info on error messages, check: '
-            'https://cloud.google.com/apis/design/errors'.format(
-                response.error.message))
-
-def main():
-    file_name = os.path.abspath('wakeupcat.jpg')
-    detect_text(file_name)
-
-if __name__ == "__main__":
-    main()
+    #     if response.error.message:
+    #         raise Exception(
+    #             '{}\nFor more info on error messages, check: '
+    #             'https://cloud.google.com/apis/design/errors'.format(
+    #                 response.error.message))
